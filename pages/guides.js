99f1d80cf7b7f57cfa21bfd7d9046aadb7963a6a
mohamed <mohamed.mohamed.yossef@gmail.com>
2021-05-17 16:01:53 +0200

    add serverless functions

--- pages/guides.js
+++ pages/guides.js
@@ -1,7 +1,39 @@
 import styles from "../styles/Guides.module.css"
 import Head from "next/head"
+import { useContext, useEffect, useState } from "react"
+import AuthContext from "../context/authContext"
 
 const Guides = () => {
+  const { user, authReady, login } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
+  /*useEffect(() => {
+    if (authReady) {
+      fetch(
+        "",
+        user && {
+          headers: {
+            Authorization: "Bearer " + user.token.access_token,
+          },
+        }
+      )
+        .then(res => {
+          if (!res.ok) {
+            login()
+            throw Error("You must login to view the content")
+          }
+          return res.json()
+        })
+        .then(data => {
+          setGuides(data)
+          setError(null)
+        })
+        .catch(err => {
+          setGuides(null)
+          setError(err.message)
+        })
+    }
+  }, [user, authReady])*/
   return (
     <div>
       <Head>
